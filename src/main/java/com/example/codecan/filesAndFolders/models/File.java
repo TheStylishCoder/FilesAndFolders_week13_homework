544c03136e9ab74cd3cc3cf2e426f29cee77802b
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-30 15:48:09 +0100

    added annotations to File class

--- src/main/java/com/example/codecan/filesAndFolders/models/File.java
+++ src/main/java/com/example/codecan/filesAndFolders/models/File.java
@@ -1,11 +1,26 @@
 package com.example.codecan.filesAndFolders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
 public class File {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "extension")
     private String extension;
+    @Column(name = "size")
     private int size;
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    @JsonIgnoreProperties({"files"})
     private Folder folder;
 
     public File(String name, String extension, int size, Folder folder) {
