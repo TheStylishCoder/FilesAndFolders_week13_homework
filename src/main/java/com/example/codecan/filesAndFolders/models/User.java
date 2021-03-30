01f7b62b34b2ee06e3becf70a2d39772685a7de9
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-30 16:07:14 +0100

    created basic user class

--- src/main/java/com/example/codecan/filesAndFolders/models/User.java
+++ src/main/java/com/example/codecan/filesAndFolders/models/User.java
@@ -1,4 +1,44 @@
 package com.example.codecan.filesAndFolders.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private Long id;
+    private String name;
+    private List<Folder> folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<Folder>();
+    }
+
+    public User(){
+        
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
