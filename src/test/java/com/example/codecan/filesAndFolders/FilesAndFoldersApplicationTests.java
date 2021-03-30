ace40c93150d3ead3bfc622cd432d8f3b1e87124
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-30 15:57:11 +0100

    created basic folder class

--- src/test/java/com/example/codecan/filesAndFolders/FilesAndFoldersApplicationTests.java
+++ src/test/java/com/example/codecan/filesAndFolders/FilesAndFoldersApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.codecan.filesAndFolders;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FilesAndFoldersApplicationTests {
 
+
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
