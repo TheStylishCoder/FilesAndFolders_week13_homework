545d9f785253a44f5e2617adad813d1fc0d58839
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-30 18:01:18 +0100

    tested routes for file with insomnia

---
+++ src/main/java/com/example/codecan/filesAndFolders/controllers/FileController.java
@@ -0,0 +1,35 @@
+package com.example.codecan.filesAndFolders.controllers;
+
+import com.example.codecan.filesAndFolders.models.File;
+import com.example.codecan.filesAndFolders.models.Folder;
+import com.example.codecan.filesAndFolders.repositories.FileRepository;
+import com.example.codecan.filesAndFolders.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FileController {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles(){
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity getFile(@PathVariable Long id){
+        return new ResponseEntity<>(fileRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/files")
+    public ResponseEntity<File> postFile(@RequestBody File file){
+        fileRepository.save(file);
+        return new ResponseEntity<>(file, HttpStatus.CREATED);
+    }
+}
